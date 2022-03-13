92e70c03cb59283d0c11e6b775fd90d1dee486b3
Yuwei Zhang <ywzhang224@gmail.com>
2022-03-13 21:15:08 +0800

    modif on integrate

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """
 Time:     2022/3/7 15:37
 Author:   ZHANG Yuwei
-Version:  V 0.1
+Version:  V 0.2
 File:     setup.py
 Describe:
 """
@@ -10,15 +10,16 @@ Describe:
 import setuptools
 
 # Reads the content of your README.md into a variable to be used in the setup below
-# with open("./README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
 
 setuptools.setup(
     name='supertld',
     version='0.0.2',
+    license='MIT',
     description='SuperTLD: Detecting TAD-like domains from RNA-associated interactions',
-    # long_description=long_description,  # loads your README.md
-    # long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    long_description=long_description,  # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
     author='Yu Wei Zhang',
     author_email='ywzhang224@gmail.com',
     url='https://github.com/deepomicslab/SuperTLD',
